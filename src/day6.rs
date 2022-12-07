37eeca36242156a2b453fb395aee478dba71c02d
Oscar Nylander <contact@oscarnylander.dev>
2022-12-07 20:04:16 +0100

    Day 6, Part 2

--- src/day6.rs
+++ src/day6.rs
@@ -2,13 +2,12 @@ use std::collections::HashMap;
 
 use aoc_runner_derive::aoc;
 
-#[aoc(day6, part1)]
-pub fn solve_part1(input: &str) -> u32 {
-    let mut out = 4;
+fn solve_generic(input: &str, window: usize) -> u32 {
+    let mut out = window as u32;
     let mut counts = HashMap::<char, u32>::default();
 
     let mut all_unique = true;
-    for c in input.chars().take(4) {
+    for c in input.chars().take(window) {
         let current = counts.get(&c);
         if current.is_some() {
             all_unique = false;
@@ -21,7 +20,7 @@ pub fn solve_part1(input: &str) -> u32 {
     }
 
     let mut l = input.chars();
-    let mut r = input.chars().skip(4).peekable();
+    let mut r = input.chars().skip(window).peekable();
     while r.peek().is_some() {
         out += 1;
         let left = l.next().unwrap();
@@ -42,6 +41,16 @@ pub fn solve_part1(input: &str) -> u32 {
         }
     }
     out
+}
+
+#[aoc(day6, part1)]
+pub fn solve_part1(input: &str) -> u32 {
+    solve_generic(input, 4)
+}
+
+#[aoc(day6, part2)]
+fn solve_part2(input: &str) -> u32 {
+    solve_generic(input, 14)
 }
 
 #[cfg(test)]
@@ -84,6 +93,46 @@ mod tests {
     fn test5() {
         let expected = 11;
         let actual = solve_part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test6() {
+        let expected = 19;
+        let actual = solve_part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test7() {
+        let expected = 23;
+        let actual = solve_part2("bvwbjplbgvbhsrlpgdmjqwftvncz");
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test8() {
+        let expected = 23;
+        let actual = solve_part2("nppdvjthqldpwncqszvftbrmjlhg");
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test9() {
+        let expected = 29;
+        let actual = solve_part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test10() {
+        let expected = 26;
+        let actual = solve_part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
 
         assert_eq!(expected, actual)
     }
